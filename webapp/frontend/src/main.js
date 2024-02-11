d73e20f565e0679cec831d1b9a65da9d2c5ed585
Radim Vališ <radim.valis3@gmail.com>
2024-02-11 23:38:51 +0100

    Install Vuetify

--- webapp/frontend/src/main.js
+++ webapp/frontend/src/main.js
@@ -1,8 +1,10 @@
 
 import "./assets/main.css";
+import "vuetify/styles";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,6 +12,7 @@ import router from "./router";
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(createVuetify());
 app.use(router);
 
 app.mount("#app");
--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -1,19 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  server: {
-    port: parseInt(process.env.FRONTEND_PORT),
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+    plugins: [
+        vue(),
+        vuetify()
+    ],
+    server: {
+        port: parseInt(process.env.FRONTEND_PORT),
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
     }
-  }
-})
+});
