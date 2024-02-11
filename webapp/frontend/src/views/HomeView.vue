9d5f40de4b11c5794071b09eb27ae8efb4421f3a
Radim Vališ <radim.valis3@gmail.com>
2024-02-11 20:07:43 +0100

    Clean up frontend

--- webapp/frontend/src/views/HomeView.vue
+++ webapp/frontend/src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+    <h1>This is a home page</h1>
+
 </template>
