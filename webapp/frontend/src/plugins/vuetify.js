32d9247aaf1a0adca72cfdbed43cb0bbe9c53edd
Radim Vališ <radim.valis3@gmail.com>
2024-02-17 19:45:52 +0100

    Create custom theme

--- webapp/frontend/src/plugins/vuetify.js
+++ webapp/frontend/src/plugins/vuetify.js
@@ -3,6 +3,32 @@ import "vuetify/styles";
 
 import { createVuetify } from "vuetify";
 
-const vuetify = createVuetify();
+const customLightTheme = {
+
+    dark: false,
+    colors: {
+
+        background: "#F2F2F2",
+        surface: "#FFFFFF",
+        primary: "#336699",
+        secondary: "#A69D6D",
+        error: "#B00020",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+    },
+};
+
+const vuetify = createVuetify({
+
+    theme: {
+
+        defaultTheme: "customLightTheme",
+        themes: {
+
+            customLightTheme
+        },
+    },    
+});
 
 export default vuetify;
