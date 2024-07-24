a12f16feaa7b15228eac8e8ca37b804810823272
Radim Vališ <radim.valis3@gmail.com>
2024-07-24 15:13:32 +0200

    Add progress dialog and error dialog

---
+++ webapp/frontend/src/components/ConnectToBoardProgressDialog.vue
@@ -0,0 +1,44 @@
+
+<script setup>
+
+    const model = defineModel({ default: false });
+
+</script>
+
+<template>
+
+    <v-dialog
+        v-model="model"
+        persistent
+        width="300"
+    >
+
+        <v-list
+            rounded="lg"
+        >
+
+            <v-list-item>
+
+                <v-list-item-title>
+
+                    Connecting to board ...
+
+                </v-list-item-title>
+
+                <template #append>
+
+                    <v-progress-circular
+                        indeterminate
+                        :size="40"
+                        color="primary"
+                    />
+
+                </template>
+
+            </v-list-item>
+
+        </v-list>
+
+    </v-dialog>
+
+</template>
